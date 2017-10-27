729dce89fd4292e33788657f1950f96016375428
Looney_gongsidiannao <250384247@qq.com>
2017-10-27 16:48:45 +0800

    dasd
    dasdas

--- app/src/main/java/net/suntrans/guojj/activity/AboutActivity.java
+++ app/src/main/java/net/suntrans/guojj/activity/AboutActivity.java
@@ -14,6 +14,7 @@ import net.suntrans.guojj.R;
 
 public class AboutActivity extends BasedActivity {
     private TextView guangwang;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +26,8 @@ public class AboutActivity extends BasedActivity {
                 finish();
             }
         });
-        TextView textView = (TextView)findViewById(R.id.version);
-        textView .setText(getString(R.string.tx_version_code)+ BuildConfig.VERSION_NAME);
+        TextView textView = (TextView) findViewById(R.id.version);
+        textView.setText(getString(R.string.tx_version_code) + BuildConfig.VERSION_NAME);
         findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,14 +41,13 @@ public class AboutActivity extends BasedActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public  void share(String desc)
-    {
+    public void share(String desc) {
 
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.putExtra(Intent.EXTRA_SUBJECT,"");
-        shareIntent.putExtra(Intent.EXTRA_TEXT,  getString(R.string.tx_share_app_des));
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.tx_share_app_des));
         shareIntent.setType("text/plain");
-       startActivity(Intent.createChooser(shareIntent, desc));
+        startActivity(Intent.createChooser(shareIntent, desc));
     }
 
 
